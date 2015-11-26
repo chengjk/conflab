934c950274a34c1be7021ce55d9dc97c5ee1662c
jacky <jacky.cheng@yeahmobi.com>
2015-11-26 23:49:27 +0800

    dev appids

--- conflab-client/src/main/java/com/jk/conflab/client/DefaultConfLabInit.java
+++ conflab-client/src/main/java/com/jk/conflab/client/DefaultConfLabInit.java
@@ -1,8 +1,15 @@
 package com.jk.conflab.client;
 
 
+import com.jk.conflab.client.utils.ConfConstants;
+import com.jk.conflab.client.utils.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Set;
 
 /**
  * Created by Administrator on 2015/11/25.
@@ -12,13 +19,31 @@ public abstract class DefaultConfLabInit {
     public static Boolean isDev=true;
     public DefaultConfLabInit() {
         if (isDev) {
-            //// TODO: 2015/11/25  从环境中取得配置应用id，注册。可以是多个。
             logger.info( "is dev");
+            Set<Object> devAppIds = getDevAppIds();
         }else{
             logger.info( "not dev,appId:{}" + getAppId());
             //注册当前应用  。
             ConfLab.register(getAppId());
         }
     }
+
+    private Set<Object> getDevAppIds() {
+        String config_home = System.getenv("CONFIG_HOME");
+        if (!StringUtils.hasText(config_home)) {
+            config_home = System.getProperty("user.home");
+            System.err.println("DEV MODEL Read EVN VAR 'CONFIG_HOME' IS NULL,USE HOME " + config_home);
+        }
+        String path = config_home + ConfConstants.DevConfigFileName;
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream(path));
+        } catch (IOException e) {
+            logger.error("开发环境，读取配置id文件错误。" + e.getMessage(), e);
+            e.printStackTrace();
+        }
+        return properties.keySet();
+    }
+
     protected abstract String getAppId();
 }
--- conflab-client/src/main/java/com/jk/conflab/client/utils/ConfConstants.java
+++ conflab-client/src/main/java/com/jk/conflab/client/utils/ConfConstants.java
@@ -4,5 +4,12 @@ package com.jk.conflab.client.utils;
  * Created by Administrator on 2015/11/25.
  */
 public class ConfConstants {
+    /**
+     * 配置存放的zookeeper跟目录
+     */
     public static final String ZkConfigRoot = "/conflab/config";
+    /**
+     * 开发环境下，appIds保存文件
+     */
+    public static final String DevConfigFileName = "conflab.properties";
 }
