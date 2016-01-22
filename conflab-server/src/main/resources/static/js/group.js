79a24d3bc7e1da737259a542432940846d5dc14d
jacky <jacky.cheng@yeahmobi.com>
2016-01-22 13:57:16 +0800

    add new group

--- conflab-server/src/main/resources/static/js/group.js
+++ conflab-server/src/main/resources/static/js/group.js
@@ -3,12 +3,12 @@ define(['jquery','_','config','mockdata'], function ($,_,config) {
     return {
         init: function () {
             self = this;
+            self.initView();
         },
         loadData:function(appId){
             console.log("loadGroup");
             var url = "/group/app/"+appId;
             $.getJSON(url, function (datas) {
-                console.log(datas);
                 $.get("temp/grouptab.html",function(temp){
                     console.log(temp);
                     var t = _.template(temp,{ 'variable': 'datas'});
@@ -19,15 +19,26 @@ define(['jquery','_','config','mockdata'], function ($,_,config) {
             });
         },
         initView:function(){
-             $("#tabConfig").parent().addClass("hidden");
-             $("#tabGroup").parent().removeClass("hidden");
-             $("#tabGroup tr").on("click",function(e){
-                self.open($(this).data("id"))
-             });
+            $("#tabConfig").parent().addClass("hidden");
+            $("#tabGroup").parent().removeClass("hidden");
+            $("#tabGroup tr").on("click",function(e){
+               self.open($(this).data("id"))
+            });
+            var form=$("#tabGroup").next(".form-inline");
+            form.find("button").on("click",function(){
+                self.add(form);
+            })
         },
         open:function(id){
             config.init();
             config.open(id);
+        },
+        add:function(form){
+            form.find("input[name=appId]").val(1); //todo here
+            console.log(form.serialize());
+            $.post("/group/add",form.serialize(),function(e){
+                console.log(e)
+            })
         }
     }
 });
